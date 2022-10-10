2182eb1040a6599fbd99fa82fd98f2d85e3d28a8
Bhaveshgithub0595 <78193214+Bhaveshgithub0595@users.noreply.github.com>
2022-10-10 19:03:08 +0530

    Add files via upload

--- Find the greater element.cpp
+++ Find the greater element.cpp
@@ -1,34 +1,48 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void printNextGreaterElement(int nums[],int n){
-  stack<int> s;
-    int res[n];
-    for (int i = 2 * n - 1; i >= 0; i--)
-    {
 
-        while (!s.empty() && nums[s.top()] <= nums[i % n])
-        {
-            s.pop();
-        }
-
-        res[i % n] = (s.empty() ? -1 : nums[s.top()]);
-        s.push(i % n);
-    }
-    for (int i = 0; i < n; i++) {
-        cout << res[i] << " ";
-    }
-}
 int main()
 {
-  
     int n;
     cin>>n;
-    int arr[n];
-   
-    for(int i=0;i<n;i++)
-      cin>>arr[i];
-    printNextGreaterElement(arr,n);
     
-  return 0;
-}
+    int arr[n];
+    
+    for(int i=0;i<n;i++)cin>>arr[i];
 
+    
+    vector<int>ans;
+    
+    stack<int>s;
+    
+    s.push(-1);
+    
+    for(int i=n-1;i>=0;i--){
+        s.push(i);
+    }
+    
+    for(int i=n-1;i>=0;i--){
+        
+        while(!s.empty() && s.top()!=-1 && arr[s.top()]<=arr[i])s.pop();
+        
+        ans.push_back(s.top());
+        
+        s.push(i);
+        
+    }
+    
+    reverse(ans.begin(),ans.end());
+    
+    for(int i=0;i<n;i++){
+        if(ans[i]!=-1)
+        cout<<arr[ans[i]]<<" "; 
+        
+        else{
+            cout<<-1<<" ";
+        }
+    }cout<<endl;
+    
+
+    return 0;
+}
